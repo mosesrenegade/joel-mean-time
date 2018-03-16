3354d5adfc57810ef0b94ce01ed0c70479b72408
Moses Frost <moses@moses.io>
2018-03-16 11:43:03 -0400

    Open not to localhost

--- app.py
+++ app.py
@@ -39,4 +39,4 @@ if __name__ == '__main__':
     #if os.environ.get('APP_LOCATION') == 'heroku':
     #    run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
     #else:
-    run(host='localhost', port=8080, debug=True)
+    run(host="0.0.0.0", port=8080, debug=True)
