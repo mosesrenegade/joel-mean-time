fc35d34231b682d133f9497086a0cf64f7a07302
Moses Frost <moses@moses.io>
2018-03-19 21:57:54 -0400

    Gunicorn?

--- app.py
+++ app.py
@@ -39,4 +39,4 @@ if __name__ == '__main__':
     #if os.environ.get('APP_LOCATION') == 'heroku':
     #    run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
     #else:
-    run(host="0.0.0.0", port=8080, debug=True)
+    run(host="0.0.0.0", port=8080, server=gunicorn, debug=True)
